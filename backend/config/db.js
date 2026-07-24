3f3faa72ace0ffc60dab39f775cd77f38d948b8d
Cremefreesh <archieeknight@gmail.com>
2026-07-24 03:09:56 +0100

    db basic setup

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,3 +3,23 @@
 //if less than highscore, do nothing
 //else put the current score to the database (as no score exists for this playerID)
 
+const images = [
+  { id: 1, name: "Image1", charIDs: [1, 2] },
+  { id: 2, name: "Image2", charIDs: [3, 4] },
+];
+
+const chars = [
+  { id: 1, name: "Goku", cordinates: { x: 10, y: 20 } },
+  { id: 2, name: "Gohan", cordinates: { x: 30, y: 40 } },
+  { id: 3, name: "Vegeta", cordinates: { x: 50, y: 60 } },
+  { id: 4, name: "Piccolo", cordinates: { x: 70, y: 80 } },
+];
+
+
+
+async function getCharPositionById(charId) {
+  return chars.find(char => char.cordinates.x && char.cordinates.y && char.id === charId);
+};
+
+
+module.exports = { getCharPositionById };
